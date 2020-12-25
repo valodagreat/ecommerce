64c81710c1f7c552648f17b072b7b4ab3525b345
caleb1-valo <65986150+caleb1-valo@users.noreply.github.com>
2020-12-25 01:16:05 +0100

    Ready for deployment

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://sheltered-wave-91250.herokuapp.com",
 });
 
 export default instance;
