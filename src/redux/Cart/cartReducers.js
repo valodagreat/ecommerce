815dcb02aaec421f7f8473e4afc759b2d3a86ab0
caleb1-valo <65986150+caleb1-valo@users.noreply.github.com>
2020-12-25 01:54:15 +0100

    Ready for deployment

--- src/redux/Cart/cartReducers.js
+++ src/redux/Cart/cartReducers.js
@@ -1,3 +1,4 @@
+import { CART_CLEAR_ITEMS } from "../Order/orderTypes";
 import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "./cartTypes";
 
 export const cartReducer = (state={cartItems: [], shippingAddress: {}}, action) => {
@@ -32,6 +33,11 @@ export const cartReducer = (state={cartItems: [], shippingAddress: {}}, action) 
             return {
                 ...state,
                 paymentMethod: action.payload
+            }
+        case CART_CLEAR_ITEMS:
+            return {
+                ...state,
+                cartItems: [],
             }
         default:
             return state
